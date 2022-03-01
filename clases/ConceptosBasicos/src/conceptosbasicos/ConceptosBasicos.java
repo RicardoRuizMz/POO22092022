7e2dcc419805ac9df07666e56918636e8a3b20e5
RicardoRuizMz <ricardo.ruiz00@comunidad.unam.mx>
2022-03-01 12:51:00 -0600

    Sesion 01/03/2022, Automovil

--- clases/ConceptosBasicos/src/conceptosbasicos/ConceptosBasicos.java
+++ clases/ConceptosBasicos/src/conceptosbasicos/ConceptosBasicos.java
@@ -4,6 +4,10 @@
  */
 package conceptosbasicos;
 
+import ico.fes.Automovil;
+import ico.fes.silla;
+import java.awt.Color;
+
 /**
  *
  * @author Ricardo
@@ -16,6 +20,22 @@ public class ConceptosBasicos {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hola de nuevo");
+        silla s1=new silla();
+        System.out.println(s1);
+        System.out.println(s1.toString());
+        
+        Automovil bocho=new Automovil();
+        bocho.setMarca("VM");
+        bocho.setSubMarca("Sedan");
+        bocho.setModelo(1970);
+        bocho.setColor(Color.blue);
+        System.out.println(bocho);
+        
+        Automovil Mustang=new Automovil("Ford", "Mustang", 2010, Color.yellow);
+        System.out.println(Mustang);
+        
+        Automovil Akura=new Automovil("Akura", "NSX", 2013, Color.GRAY);
+        System.out.println(Akura);
     }
     
 }
