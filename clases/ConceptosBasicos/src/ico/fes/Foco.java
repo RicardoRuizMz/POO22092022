b3d4733b8465dcae76bdaef419dc01ff5ae2ca81
RicardoRuizMz <ricardo.ruiz00@comunidad.unam.mx>
2022-03-01 01:43:55 -0600

    Iniciamos el archivo Foco.java

---
+++ clases/ConceptosBasicos/src/ico/fes/Foco.java
@@ -0,0 +1,60 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ico.fes;
+
+/**
+ *
+ * @author Ricardo
+ */
+public class Foco {
+    private String color;
+    private String tipo;
+    private int tamano;
+
+    public Foco() {
+    }
+
+    public Foco(String color, String tipo, int tamano) {
+        this.color = color;
+        this.tipo = tipo;
+        this.tamano = tamano;
+    }
+
+    public int getTamano() {
+        return tamano;
+    }
+
+    public void setTamano(int tamano) {
+        this.tamano = tamano;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Foco{");
+        sb.append("color=").append(color);
+        sb.append(", tipo=").append(tipo);
+        sb.append(", tamano=").append(tamano);
+        sb.append('}');
+        return sb.toString();
+    }
+    
+}
